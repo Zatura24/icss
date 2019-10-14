15f98ea983f12772d277c1f6c27487a673c46bcb
Jannick Joosten <Jannick.Joosten@gmail.com>
2019-10-14 16:13:40 +0200

    Generator voor level 0 geïmplementeerd

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -5,6 +5,38 @@ import nl.han.ica.icss.ast.*;
 public class Generator {
 
 	public String generate(AST ast) {
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+		for (ASTNode node:
+			 ast.root.getChildren()) {
+			stringBuilder.append(generate(node));
+		}
+
+		return stringBuilder.toString();
+	}
+
+	private String generate(ASTNode node) {
+		StringBuilder stringBuilder = new StringBuilder();
+
+		if (node instanceof Stylerule) {
+			for (Selector selector :
+					((Stylerule) node).selectors) {
+					stringBuilder.append(selector.toString()).append(" ");
+			}
+
+			stringBuilder.append("{\r\n");
+
+			for (ASTNode declaration :
+					((Stylerule) node).body) {
+				if (declaration instanceof Declaration) {
+					stringBuilder.append("\t").append(((Declaration) declaration).property.name).append(": ");
+					stringBuilder.append(((Declaration) declaration).expression.toString()).append(";\r\n");
+				}
+			}
+
+			stringBuilder.append("}\r\n\r\n");
+		}
+
+		return stringBuilder.toString();
 	}
 }
